5fb1ae71df8411ed37bfd7bc3b39b2f26513857b
benitol87 <benoit.leroy@etu.unilim.fr>
2015-11-03 15:21:45 +0100

    Remplissage de boite.ads

--- boite.ads
+++ boite.ads
@@ -1,4 +1,17 @@
 package boite is
    -- Contient des méthodes pour créer une boite
-   -- type boite?
+
+   type Boite is record
+	t: Natural; -- epaisseur
+	l: Natural; -- longueur
+	w: Natural; -- largeur
+	q: Natural; -- longueurQueue
+	h: Natural; -- hauteur
+	b: Natural; -- hauteur interne (doit vérifier b<h-2t)
+	nomFichier: String(1..100); -- nom du fichier limité à 100 caracteres
+   end record;
+
+   -- Cree un fichier SVG a partir des données d'une boite
+   procedure creerBoite(b: Boite);
+
 end;
